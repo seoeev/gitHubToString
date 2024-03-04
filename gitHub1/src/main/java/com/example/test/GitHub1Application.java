22df4468833502fecf7f6980e07ed63ca8048836
seoeev <mlmcild0905@gmail.com>
2024-03-04 16:15:22 +0900

    asdf

---
+++ gitHub1/src/main/java/com/example/test/GitHub1Application.java
@@ -0,0 +1,13 @@
+package com.example.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitHub1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitHub1Application.class, args);
+	}
+
+}
